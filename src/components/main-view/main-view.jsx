55fc8eed6a6102c3c0184196b08b9c1b36ab6008
Eloi-Perez <eloi104@hotmail.com>
2021-04-30 16:11:54 +0100

    first hook + propTypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -9,13 +12,22 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Primer', Description: 'desc1...', ImagePath: 'https://movie-api2.herokuapp.com/img/primer' },
-                { _id: 2, Title: 'Looper', Description: 'desc2...', ImagePath: 'https://movie-api2.herokuapp.com/img/looper' },
-                { _id: 3, Title: 'Arrival', Description: 'desc3...', ImagePath: 'https://movie-api2.herokuapp.com/img/arrival' }
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         }
+    }
+
+    componentDidMount() {
+        axios.get('https://movie-api2.herokuapp.com/movies/featured')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -24,22 +36,33 @@ export class MainView extends React.Component {
         });
     }
 
+    /* After login, updates 'user' in state */
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
     render() {
         const { movies, selectedMovie } = this.state;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+        if (!this.state.user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
                     ))
                 }
             </div>
         );
-        
+
     }
 
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={ `https://movie-api2.herokuapp.com${movie.ImagePath}` } />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
