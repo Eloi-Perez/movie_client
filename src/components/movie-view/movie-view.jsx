cd943eedec170f7f8c02cf2d922ee608356c6098
Eloi-Perez <eloi104@hotmail.com>
2021-05-09 20:23:24 +0100

    fix error "score=null"

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -32,6 +32,10 @@ export function MovieView(props) {
         setScore(numVal);
     }
 
+    const scoreFunc = () => {
+        if (score) {return score};
+    }
+
     const saveProperties = (e) => {
         e.preventDefault();
         console.log('saving...');
@@ -88,7 +92,7 @@ export function MovieView(props) {
                 /></Form.Group>
                 <Form.Group controlId="formScore">
                     <Form.Label>Score</Form.Label>
-                    <Form.Control as="select" value={score} onChange={e => switchScore(e.target.value)}>
+                    <Form.Control as="select" value={scoreFunc()} onChange={e => switchScore(e.target.value)}>
                         <option>0</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option>
                     </Form.Control>
                 </Form.Group>
