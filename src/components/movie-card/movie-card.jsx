61e6550625c1c9a3cf278d0c7c2f30a55063a93e
Eloi-Perez <eloi104@hotmail.com>
2021-05-09 20:24:23 +0100

    remove some propTypes to reuse component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -40,13 +40,13 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired
-        }),
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired
-        }),
+        // Description: PropTypes.string.isRequired,
+        // Genre: PropTypes.shape({
+        //     Name: PropTypes.string.isRequired
+        // }),
+        // Director: PropTypes.shape({
+        //     Name: PropTypes.string.isRequired
+        // }),
         ImagePath: PropTypes.string.isRequired
     }).isRequired
     // onMovieClick: PropTypes.func.isRequired
