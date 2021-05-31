61f1948ec155ef35bb3b4e8c5d4f492a7306b4dd
Eloi-Perez <eloi104@hotmail.com>
2021-05-31 22:41:40 +0100

    bootstrap container moved to main-view

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
@@ -8,27 +7,23 @@ import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import MainView from './components/main-view/main-view';
 
-
-// Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
 const store = createStore(moviesApp, devToolsEnhancer());
 
-// Main component (will eventually use all the others)
+// Main component
 class MyFlixApplication extends React.Component {
   render() {
     return(
      <Provider store={store}>
-        <Container>
           <MainView />
-        </Container>
       </Provider>
     );
   }
 }
 
-// Find the root of our app
+// Find the root
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tell React to render our app in the root DOM element
+// Render app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
